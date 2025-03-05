1ad764fb5f2c8895d79cfe94fc25192569ac3f39
melkeydev <amokstakov@gmail.com>
2025-03-05 14:41:06 -0800

    Adding workout store

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,21 +1,45 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/melkeydev/femProject/internal/api"
+	"github.com/melkeydev/femProject/internal/store"
+	"github.com/melkeydev/femProject/migrations"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	// our stores will go here
+
+	// our handlers will go here
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
-		Logger: logger,
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 
 	return app, nil
